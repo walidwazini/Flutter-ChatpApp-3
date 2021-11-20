6d450c1f83126d48d64af273ba2324597155d78b
Walid Wazini <wazini25@gmail.com>
2021-11-21 07:18:39 +0800

    animation updates

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -10,11 +10,52 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation? animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+      upperBound: 1,
+    );
+
+    //  animation = CurvedAnimation(parent: controller!, curve: Curves.easeIn);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller!);
+
+    controller!.forward();
+
+    // animation!.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller!.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller!.forward();
+    //   }
+    // });
+
+    controller!.addListener(() {
+      setState(() {});
+      print(animation!.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation!.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -24,13 +65,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             Row(
               children: <Widget>[
                 Hero(
-                  tag: 'logo' ,
+                  tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: 60,
                   ),
                 ),
                 Text(
+                  //'${controller!.value.toInt()}',
                   'Flash Chat',
                   style: TextStyle(
                     fontSize: 45.0,

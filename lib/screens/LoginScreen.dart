7f10f4707c3d115d529994fc2d0cd671af9fe469
Walid Wazini <wazini25@gmail.com>
2021-11-21 18:46:00 +0800

    firebase config

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -27,69 +27,71 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: _hiddenPass,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your Password',
-                suffixIcon: InkWell(
-                    onTap: _togglePassView,
-                    child: _hiddenPass? Icon(Icons.visibility_off) : Icon(Icons.visibility)
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Log In ', colour: Colors.pinkAccent,
-              onPressed: (){
-
-              },
-            ),
-            SizedBox(height: 10,),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: TextButton(
-                    child: Text('Go Back',),
-                    onPressed: (){
-                      Navigator.of(context).pop();
-                    },
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: _hiddenPass,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your Password',
+                  suffixIcon: InkWell(
+                      onTap: _togglePassView,
+                      child: _hiddenPass? Icon(Icons.visibility_off) : Icon(Icons.visibility)
                   ),
                 ),
-                Spacer(flex: 2,)
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Log In ', colour: Colors.pinkAccent,
+                onPressed: (){
+
+                },
+              ),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: TextButton(
+                      child: Text('Go Back',),
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                  Spacer(flex: 2,)
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

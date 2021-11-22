9cf54fc10fb667248cfc07ba49e55581fa0f65ca
Walid Wazini <wazini25@gmail.com>
2021-11-22 11:41:53 +0800

    logout and loading

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../widgets/RoundedButton.dart';
+import '../screens/ChatScreen.dart';
 import '../constants.dart';
 import './WelcomeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   String? email;
   String? password;
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  final _auth = FirebaseAuth.instance;
 
   bool _hiddenPass = true;
 
-
-  void _togglePassView(){
-    if (_hiddenPass == true){
+  void _togglePassView() {
+    if (_hiddenPass == true) {
       _hiddenPass = false;
     } else {
       _hiddenPass = true;
@@ -49,17 +54,19 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 48.0,
               ),
               TextField(
+                controller: emailController,
                 keyboardType: TextInputType.emailAddress,
                 onChanged: (value) {
                   email = value;
                 },
-                
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
               ),
               SizedBox(
                 height: 8.0,
               ),
               TextField(
+                controller: passwordController,
                 obscureText: _hiddenPass,
                 onChanged: (value) {
                   //Do something with the user input.
@@ -68,32 +75,43 @@ class _LoginScreenState extends State<LoginScreen> {
                   hintText: 'Enter your Password',
                   suffixIcon: InkWell(
                       onTap: _togglePassView,
-                      child: _hiddenPass? Icon(Icons.visibility_off) : Icon(Icons.visibility)
-                  ),
+                      child: _hiddenPass
+                          ? Icon(Icons.visibility_off)
+                          : Icon(Icons.visibility)),
                 ),
               ),
-              SizedBox(
-                height: 24.0,
-              ),
+              SizedBox(height: 24.0),
               RoundedButton(
-                title: 'Log In ', colour: Colors.pinkAccent,
-                onPressed: (){
-
-                },
-              ),
-              SizedBox(height: 10,),
+                  title: 'Log In ',
+                  colour: Colors.pinkAccent,
+                  onPressed: () async {
+                    final user = (await _auth.signInWithEmailAndPassword(
+                            email: emailController.text,
+                            password: passwordController.text))
+                        .user;
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } else {
+                      print('Error');
+                    }
+                  }),
+              SizedBox(height: 10),
               Row(
                 children: [
                   Expanded(
                     flex: 1,
                     child: TextButton(
-                      child: Text('Go Back',),
-                      onPressed: (){
+                      child: Text(
+                        'Go Back',
+                      ),
+                      onPressed: () {
                         Navigator.of(context).pop();
                       },
                     ),
                   ),
-                  Spacer(flex: 2,)
+                  Spacer(
+                    flex: 2,
+                  )
                 ],
               )
             ],
--- lib/screens/RegisterScreen.dart
+++ lib/screens/RegisterScreen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app_2/widgets/RoundedButton.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 import './ChatScreen.dart';
 import '../constants.dart';
@@ -15,7 +16,7 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
 
   final _auth = FirebaseAuth.instance;
-
+  bool showLoading = false;
   String? email;
   String? password;
 
@@ -35,58 +36,66 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(24, 40, 24, 10),
-          child: Column( mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
-              ),
-              SizedBox(height: 48.0),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                 email = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
-              ),
-              SizedBox(
-                height: 28.0,
-              ),
-              TextField(
-                obscureText: _hiddenPass,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your Password',
-                  suffixIcon: InkWell(
-                      onTap: _togglePassView,
-                      child: _hiddenPass? Icon(Icons.visibility_off) : Icon(Icons.visibility)
+        child: LoadingOverlay(
+          color: Colors.black,
+          isLoading: showLoading,
+          progressIndicator: CircularProgressIndicator(backgroundColor: Colors.pink,),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(24, 40, 24, 10),
+            child: Column( mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
                   ),
                 ),
-              ),
-              SizedBox(height: 24.0,),
-             RoundedButton(
-               title: 'Register', colour: Colors.red,
-               onPressed: () async {
-                 try {
-                   final newUser = await _auth.createUserWithEmailAndPassword(
-                       email: email!,
-                       password: password!
-                   );
-                   Navigator.pushNamed(context, ChatScreen.id);
-                 } catch (e) {
-                   print(e);
-                 }
+                SizedBox(height: 48.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                   email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+                ),
+                SizedBox(
+                  height: 28.0,
+                ),
+                TextField(
+                  obscureText: _hiddenPass,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your Password',
+                    suffixIcon: InkWell(
+                        onTap: _togglePassView,
+                        child: _hiddenPass? Icon(Icons.visibility_off) : Icon(Icons.visibility)
+                    ),
+                  ),
+                ),
+                SizedBox(height: 24.0,),
+               RoundedButton(
+                 title: 'Register', colour: Colors.red,
+                 onPressed: () async {
+                   setState(() {
+                     showLoading = true;
+                   });
+                   try {
+                     final newUser = await _auth.createUserWithEmailAndPassword(
+                         email: email!,
+                         password: password!
+                     );
+                     Navigator.pushNamed(context, ChatScreen.id);
+                   } catch (e) {
+                     print(e);
+                   }
 
-               },
-             )
-            ],
+                 },
+               )
+              ],
+            ),
           ),
         ),
       ),

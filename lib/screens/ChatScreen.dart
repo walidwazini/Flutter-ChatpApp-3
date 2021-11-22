c49f523a45f5d40c3180b6ee79cd05abc8b2aa0e
Walid Wazini <wazini25@gmail.com>
2021-11-22 12:44:05 +0800

    message to cloud firestore

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../constants.dart';
 
@@ -13,7 +14,10 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   //final FirebaseAuth?  _auth = FirebaseAuth.instance;
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+
   User? loggedInUser;
+  String?  messageText;
 
   @override
   void initState() {
@@ -63,14 +67,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      // messageText + loggedInUser.email
+                      _firestore.collection('messages').add({
+                        'text' : messageText,
+                        'sender' : loggedInUser!.email,
+                      });
                     },
                     child: Text(
                       'Send',
